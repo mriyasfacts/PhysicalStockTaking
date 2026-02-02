34542a2ab3b74b33f5b043edaca3abcffcdc54e6
mriyasfacts <mriyasfacts@gmail.com>
2026-02-02 13:55:06 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.APP_CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbxHljuthdb2N4jzuZ4zW_SIC-1j2BIAChyI_eXR0AqN-gF-dpms57xml7vNmzdGWifk_Q/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbxHljuthdb2N4jzuZ4zW_SIC-1j2BIAChyI Wifk_Q/exec"
+
 };
